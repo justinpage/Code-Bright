a227ea70a029b40652549a1aa95b3f01491489c6
Justin Page <xjustinpagex@gmail.com>
2014-04-21 13:39:43 -0700

    changed creds

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'CodeBright',
 			'username'  => 'root',
-			'password'  => 'battosai',
+			'password'  => 'klvtz',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
